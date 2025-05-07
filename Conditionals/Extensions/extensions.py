0c4f1967a1ebb422002ad74343afc39f8687c4b2
Singh, Aparajita <apara.singh09@gmail.com>
2025-05-07 14:47:23 +0100

    Extensions
    to find extension of a file and provide it as output

--- Conditionals/Extensions/extensions.py
+++ Conditionals/Extensions/extensions.py
@@ -1,16 +1,23 @@
 str = input("File name: ")
-parts = str.split(".")
+parts = str.strip().split(".")
+extension = parts[len(parts) - 1].lower()
+# Check if there's an extension in the name
 if (len(parts) == 1):
+    # If there is no extension
     print("application/octet-stream")
+# If there is an extension
 else:
     extension = parts[len(parts) - 1].lower()
     if (extension == 0):
         print("application/octet-stream")
     elif (extension in ["gif", "png"]):
-        print("image/" + parts[1].lower())
+        print("image/" + extension)
     elif (extension in ["jpg", "jpeg"]):
         print("image/jpeg")
-    elif (extension in ["pdf", "txt", "zip"]):
-        print("application/" + parts[1].lower())
+    elif (extension in ["pdf", "zip"]):
+        print("application/" + extension)
+    elif (extension == "txt"):
+        print("text/plain")
+    # If the extension is not supported
     else:
         print("application/octet-stream")
