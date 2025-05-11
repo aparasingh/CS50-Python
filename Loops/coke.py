170297a7dbbd8627a94664cfa0c93b35e9f55b41
Singh, Aparajita <apara.singh09@gmail.com>
2025-05-11 23:55:52 +0100

    updated

--- Loops/coke.py
+++ Loops/coke.py
@@ -1,5 +1,13 @@
-print("Amount Due: 50")
+sum = 0
 coin = 0
-while coin < 50:
-    coin = int(input("Insert Coin: ")) + coin
-print("Change Owed: " + str(coin - 50))
+print("Amount Due: 50")
+while sum < 50:
+    while coin not in (10, 5, 25):
+        if coin != 0:
+            print("Amount Due: " + str(50 - sum))
+        coin = int(input("Insert Coin: "))
+    sum = sum + coin
+    if sum < 50:
+        print("Amount Due: "+ str(50 - sum))
+    coin = 0
+print("Change Owed: " + str(sum - 50))
