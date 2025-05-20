2f47b34852b1ab297b896598d19c6001d07f2385
Aparajita Singh <apara.singh09@gmail.com>
2025-05-20 22:09:50 +0100

    Final code

--- Loops/Vanity Plate/plates.py
+++ Loops/Vanity Plate/plates.py
@@ -7,22 +7,23 @@ def main():
 
 
 def is_valid(s):
-    if len(s)>=2 and len(s)<=6:
+    if len(s) >= 2 and len(s) <= 6:
         if s[0:2].isalpha() and s.isalnum():
-                for c in s:
-                    if c.isnumeric():
-                        if c==0:
-                            return False
+            for c in s:
+                if c.isnumeric():
+                    if int(c) == 0:
+                        return False
+                    else:
+                        part = s.split(c, 1)
+                        if part[1].isnumeric():
+                            return True
                         else:
-                            part = s.split(c)
-                            if part[1].isnumeric():
-                                return True
-                            else:
-                                return False
-                return True
+                            return False
+            return True
         else:
             return False
     else:
         return False
+
 
 main()
