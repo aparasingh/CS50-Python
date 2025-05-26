64e68344e10e899d1b09b24588d77615a0869d8f
Aparajita Singh <apara.singh09@gmail.com>
2025-05-26 14:30:17 +0100

    Updated fuel.py

--- Exception/Fuel Gauge/fuel.py
+++ Exception/Fuel Gauge/fuel.py
@@ -1,31 +1,34 @@
 def main():
-    while True:
-        x = get_fraction()
-        if x > 1:
-            pass
-        else:
-            percentage = int(round(x*100, 0))
-            if percentage >= 99:
-                print("F")
-                return
-            elif percentage <= 1:
-                print("E")
-                return
-            else:
-                print(f"{percentage}%")
-                return
+    # Main function to get fuel fraction and share fuel gauge reading
+    x = get_fraction()
+    percentage = int(round(x*100))
+    if percentage >= 99:
+        print("F")
+    elif percentage <= 1:
+        print("E")
+    else:
+        print(f"{percentage}%")
 
 
 def get_fraction():
     while True:
         try:
-            fraction = input("Fraction ")
-            values = fraction.split("/")
-            return int(values[0])/int(values[1])
+            fraction_input = input("Fraction ")
+            numerator_str, denominator_str = fraction_input.split("/")
+            numerator = int(numerator_str)
+            denominator = int(denominator_str)
+            fraction = numerator / denominator
+            # Only return valid fractions (between 0 and 1)
+            if 0 <= fraction <= 1:
+                return fraction
+            else:
+                pass  # Re-prompt on incorrect value
+        # Handle invalid input format or non-integer values
         except ValueError:
             pass
         except ZeroDivisionError:
-            pass
+            # Handle division by zero
+            continue
 
 
 main()
