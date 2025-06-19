deba5b60c301d5019addc215d13b96958ca6170a
Aparajita Singh <apara.singh09@gmail.com>
2025-06-19 21:42:53 +0100

    Updated test_bank

--- Unit Testing/test_bank/test_bank.py
+++ Unit Testing/test_bank/test_bank.py
@@ -2,15 +2,35 @@ from bank import value
 
 
 def main():
-    test_value()
+    test_hello()
+    test_smallhello()
+    test_h()
+    test_smallh()
+    test_greetings()
+    test_smallgreet()
 
 
-def test_value():
+def test_hello():
     assert value("Hello") == 0
+
+
+def test_smallhello():
     assert value("hello") == 0
+
+
+def test_h():
     assert value("Hi") == 20
+
+
+def test_smallh():
     assert value("hi") == 20
+
+
+def test_smallgreet():
     assert value("greetings") == 100
+
+
+def test_greetings():
     assert value("Greetings") == 100
 
 
