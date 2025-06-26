6791e11c2b3dfbaef6ce473c4b6882b39824e402
Aparajita Singh <apara.singh09@gmail.com>
2025-06-26 22:33:10 +0100

    Final solution

--- Files IO/Lines of code/lines.py
+++ Files IO/Lines of code/lines.py
@@ -1,6 +1,5 @@
 import sys
 
-default_path = '/workspaces/28184843/'
 if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
@@ -9,11 +8,11 @@ else:
     parts = sys.argv[1].split(".")
     if len(parts) == 2 and parts[1] == "py":
         counter = 0
-        fileName = default_path + parts[0] + '/' + sys.argv[1]
+        fileName = sys.argv[1]
         try:
             with open(fileName, "r") as file:
                 for line in file:
-                    if '#' not in line:
+                    if not line.lstrip().startswith('#') and len(line.lstrip()) != 0:
                         counter = counter + 1
                 print(counter)
                 file.close()
