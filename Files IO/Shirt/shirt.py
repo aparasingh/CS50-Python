b1eeff4b57ee5f6bc382cae203c5412c2c1380d2
Aparajita Singh <apara.singh09@gmail.com>
2025-07-03 22:33:18 +0100

    Create shirt.py

---
+++ Files IO/Shirt/shirt.py
@@ -0,0 +1,22 @@
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+else:
+    parts = sys.argv[1].split(".")
+    if len(parts) == 2 and parts[1] in ("jpg","jpeg","png"):
+        counter = 0
+        fileName = sys.argv[1]
+        try:
+            with open(fileName, "r") as file:
+                for line in file:
+                    if not line.lstrip().startswith('#') and len(line.lstrip()) != 0:
+                        counter = counter + 1
+                print(counter)
+                file.close()
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+    else:
+        sys.exit("Not a Python file")
