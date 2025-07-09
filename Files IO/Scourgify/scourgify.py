9e9e45d15a45fb6d37470728f7d3617cb4114abd
Aparajita Singh <apara.singh09@gmail.com>
2025-07-09 23:51:48 +0100

    Update scourgify.py

--- Files IO/Scourgify/scourgify.py
+++ Files IO/Scourgify/scourgify.py
@@ -15,7 +15,15 @@ else:
                     table.append(line.rstrip().split(","))
                 print(tabulate(table, headers="firstrow", tablefmt="grid"))
                 file.close()
-        except FileNotFoundError:
-            sys.exit("File does not exist")
+            with open(sys.argv[2], 'w', newline='') as csvfile:
+                fieldnames = ['first_name', 'last_name']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+                writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+                writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+                file.close()
+    except FileNotFoundError:
+        sys.exit("File does not exist")
     else:
         sys.exit("Not a Python file")
