24b346a2839121912cb7942fcc4d977a4cdb27ee
Aparajita Singh <apara.singh09@gmail.com>
2025-08-09 23:04:01 +0100

    Final scourgify

--- Files IO/Scourgify/scourgify.py
+++ Files IO/Scourgify/scourgify.py
@@ -1,29 +1,21 @@
+import csv
 import sys
-from tabulate import tabulate
 
 if len(sys.argv) < 3:
-    sys.exit("Too few command-line arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 3:
-    sys.exit("Too many command-line arguments")
-else:
-    parts = sys.argv[1].split(".")
-    if len(parts) == 2 and parts[1] == "csv":
-        table = []
-        try:
-            with open(sys.argv[1], "r") as file:
-                for line in file:
-                    table.append(line.rstrip().split(","))
-                print(tabulate(table, headers="firstrow", tablefmt="grid"))
-                file.close()
+    sys.exit("Too many arguments")
+else:åç
+    try:
+        with open(sys.argv[1], newline='') as readfile:
+            reader = csv.DictReader(readfile)
             with open(sys.argv[2], 'w', newline='') as csvfile:
-                fieldnames = ['first_name', 'last_name']
+                fieldnames = ['first', 'last', 'house']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writeheader()
-                writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-                writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-                writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-                file.close()
+                for row in reader:
+                    name = row['name']
+                    values = name.split(', ')
+                    writer.writerow({'first': values[1], 'last': values[0], 'house': row['house']})
     except FileNotFoundError:
-        sys.exit("File does not exist")
-    else:
-        sys.exit("Not a Python file")
+        sys.exit(f"Could not read {sys.argv[1]}")
