2a728ef5ef7039b8958b628fa25732d60cc7bb70
Aparajita Singh <apara.singh09@gmail.com>
2025-08-14 22:38:28 +0100

    Create watch.py
    Incomplete

---
+++ Regular Expressions/watch/watch.py
@@ -0,0 +1,24 @@
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    if re.search(r"^<iframe\s+\w+\s+\W+\"></iframe>&",s):
+        try:
+            values = s.split('src=')
+            url = values[1].split('"')[1]
+            if re.search(r"^https://www.youtube.com/embed/+[a-zA-Z0-9]&",url):
+                return url
+            else:
+                return None
+        except IndexError:
+            return None
+    else:
+        return None
+
+if __name__ == "__main__":
+    main()
