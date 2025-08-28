99b1178b5103e368750cc0f7a89b3f735c0c6df3
Aparajita Singh <apara.singh09@gmail.com>
2025-08-28 23:38:27 +0100

    Update seasons.py

--- Object Oriented Programming/seasons/seasons.py
+++ Object Oriented Programming/seasons/seasons.py
@@ -1,13 +1,31 @@
 from datetime import date
+import inflect
+import sys
 
 
 def main():
+    birth_date = get_birthdate()
+    days = get_days(birth_date)
+    minutes = get_minutes(days)
+    min_words = num_to_words(minutes)
+    print(min_words.capitalize(), "minutes")
+
+def get_birthdate():
     try:
         birth_date = input("Date of Birth: ")
-        today = date.today()
-        print(date.fromisoformat(birth_date))
+        return date.fromisoformat(birth_date)
     except ValueError:
-        print("Invalid date")
+        sys.exit("Invalid date")
+
+def get_days(birth_date):
+    return abs(date.today() - birth_date).days
+
+def get_minutes(days):
+    return days * 24 * 60
+
+def num_to_words(number):
+    p = inflect.engine()
+    return p.number_to_words(number)
 
 
 if __name__ == "__main__":
